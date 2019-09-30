584cacb3b11b096e1cc5448a2eab54e565363e8b
Neil D'souza <neil@freesoftsolutions.co.in>
2019-09-30 13:45:17 +0530

    Emacs configs

---
+++ elpa/dart-mode-20181218.2001/dart-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dart-mode" "20181218.2001" "Major mode for editing Dart files" '((emacs "24.5") (cl-lib "0.5") (dash "2.10.0") (flycheck "0.23") (s "1.10")) :commit "c51e750007e73f83f986e49cdf6154d13f1e3a8f" :keywords '("language") :authors '(("Natalie Weizenbaum")) :maintainer '("Natalie Weizenbaum") :url "https://github.com/nex3/dart-mode")
---
+++ elpa/dash-20170207.2056/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("dash.el") (22784 19976 689228 216000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
